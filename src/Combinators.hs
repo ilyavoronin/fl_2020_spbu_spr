d805a3492f9dbcfe6bf4d0d3783c9a30989b30e6
Ilya Voronin <voroninilya115@gmail.com>
2020-03-11 18:23:36 +0300

    done

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -11,28 +11,48 @@ newtype Parser error input result
   = Parser { runParser :: input -> Result error input result }
 
 instance Functor (Parser error input) where
-  fmap = error "fmap not implemented"
+  fmap f p = Parser runParser' where
+    runParser' input = case (runParser p input) of
+      Failure error         -> Failure error
+      Success input' result -> Success input' (f result)
 
 instance Applicative (Parser error input) where
-  pure = error "pure not implemented"
-  (<*>) = error "<*> not implemented"
+  pure result = Parser (\input -> Success input result)
+  (<*>) fp p = 
+    Parser runParser' where
+      runParser' input =  case (runParser fp input) of
+        Failure error          -> Failure error
+        Success input' fresult -> case (runParser p input') of
+                                   Failure error          -> Failure error
+                                   Success input'' result -> Success input'' (fresult result)
+
 
 instance Monad (Parser error input) where
-  return = error "return not implemented"
+  return = pure
 
-  (>>=) = error ">>= not implemented"
+  (>>=) p f = 
+    Parser runParser' where
+      runParser' input = case (runParser p input) of
+                           Failure error         -> Failure error
+                           Success input' result -> (runParser (f result) input')
+
 
 instance Monoid error => Alternative (Parser error input) where
-  empty = error "empty not implemented"
+  empty = Parser (\input -> Failure mempty)
 
-  (<|>) = error "<|> not implemented"
+  (<|>) p1 p2 = 
+    Parser runParser' where
+      runParser' input = case (runParser p1 input) of
+                           Failure error -> runParser p2 input
+                           ot            -> ot
 
 -- Принимает последовательность элементов, разделенных разделителем
 -- Первый аргумент -- парсер для разделителя
 -- Второй аргумент -- парсер для элемента
 -- В последовательности должен быть хотя бы один элемент
-sepBy1 :: Parser e i sep -> Parser e i a -> Parser e i [a]
-sepBy1 sep elem = error "sepBy1 not implemented"
+sepBy1 :: Monoid e => Parser e i sep -> Parser e i a -> Parser e i [a]
+sepBy1 sep elem = (:) <$> elem <*> (some (sep *> elem))
+
 
 -- Альтернатива: в случае неудачи разбора первым парсером, парсит вторым
 alt' :: Parser e i a -> Parser e i a -> Parser e i a
@@ -65,8 +85,7 @@ satisfy :: Show a => (a -> Bool) -> Parser String [a] a
 satisfy p = Parser $ \input ->
   case input of
     (x:xs) | p x -> Success xs x
-    []           -> Failure $ "Empty string"
-    (x:xs)       -> Failure $ "Predicate failed"
+    _            -> Failure $ "Predicate failed"
 
 -- Успешно парсит пустую строку
 epsilon :: Parser e i ()
